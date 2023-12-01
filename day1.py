3cac55d16adebf0538933d5010d7d5aa701568f9
Cas Visser <crbcvisser@gmail.com>
2023-12-01 06:47:25 +0100

    cleaned up day 1 solution

--- day1.py
+++ day1.py
@@ -1,25 +1,6 @@
 import aocd
 
-# inp = """two1nine
-# eightwothree
-# abcone2threexyz
-# xtwone3four
-# 4nineeightseven2
-# zoneight234
-# 7pqrstsixteen"""
-
 inp = aocd.get_data(day=1, year=2023)
-
-numbers = []
-
-# for l in inp.split("\n"):
-#     print(l)
-#     n = [c for c in l if c.isnumeric()]
-#     numbers.append(int("".join([n[0], n[-1]])))
-    
-# part1 = sum(numbers)
-# print(part1)
-# print(aocd.submit(part1, part="a", day=1, year=2023))
 
 str_digit = [
     ("0", 0),
@@ -43,26 +24,28 @@ str_digit = [
     ("eight", 8),
     ("nine", 9)]
 
+part1 = []
+part2 = []
+
 for l in inp.split("\n"):
     lmost = len(l)
     rmost = 0
     ld = -1
     rd = -1
+
+    digits = [c for c in l if c.isnumeric()]
+    part1.append(int("".join([digits[0], digits[-1]])))
+
     for s, r in str_digit:
-        pos = l.find(s)
-        if pos != -1 and pos <= lmost:
-            lmost = pos
+        lpos, rpos = l.find(s), l.rfind(s)
+        if lpos != -1 and lpos <= lmost:
+            lmost = lpos
             ld = r
-        pos = l.rfind(s)
-        if pos != -1 and pos >= rmost:
-            rmost = pos
+        if rpos != -1 and rpos >= rmost:
+            rmost = rpos
             rd = r
-
-    numbers.append(10 * ld + rd)
-
-print(numbers[-20:])
-
-part2=sum(numbers)
-print(part2)
-
-aocd.submit(part2, part="b", day=1, year=2023)
+    
+    part2.append(10 * ld + rd)
+    
+print(sum(part1))
+print(sum(part2))
