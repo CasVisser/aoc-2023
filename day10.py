0a8087a94f9b363da8fdb4938626c873d542d5fc
Cas Visser <crbcvisser@gmail.com>
2023-12-26 11:30:26 +0100

    cleaned up day 10

--- day10.py
+++ day10.py
@@ -6,17 +6,6 @@ SJ.L7
 |F--J
 LJ..."""
 
-inp = """FF7FSF7F7F7F7F7F---7
-L|LJ||||||||||||F--J
-FL-7LJLJ||||||LJL-77
-F--JF--7||LJLJ7F7FJ-
-L---JF-JLJ.||-FJLJJ7
-|F|F-JF---7F7-L7L|7|
-|FFJF7L7F-JF7|JL---7
-7-L-JL7||F7|L7F-7F7|
-L.L7LFJ|||||FJL7||LJ
-L7JLJL-JLJLJL--JLJ.L"""
-
 if len(sys.argv) > 1 and (sys.argv[1] == "gd" or sys.argv[1] == "s1" or sys.argv[1] == "s2"):
     inp = aocd.get_data(day=10, year=2023)
 
@@ -39,8 +28,9 @@ w = inp.index("\n")
 h = len(inp) // w
 s = inp.index("S")
 grid = {(i % (w + 1), i // (w + 1)): x for i, x in enumerate(inp) if x != "\n"}
-loop_coords = {(s % (w + 1), s // (w + 1))}
-queue = [(s % (w + 1), s // (w + 1))]
+start = (s % (w + 1), s // (w + 1))
+loop_coords = {start}
+queue = [start]
 
 def singly_connecting_neighbors(xy):
     return {(xy[0] + dx, xy[1] + dy) for dx, dy in connecting_dirs[grid[xy]] 
@@ -57,19 +47,14 @@ while queue:
 
 part1 = len(loop_coords) // 2
 
-def next_turn(c):
-    for x in range(c[0] + 1, w):
-        if grid[x, c[1]] in "LFSJ7":
-            return grid[x, c[1]]
-
+step_over_chars = "|JL"
+if (start[0], start[1] - 1) in doubly_connecting_neighbors(start): step_over_chars += "S"
 part2 = 0
 for y in range(h):
     inside_loop = False
     for x in range(w):
         if (x, y) in loop_coords:
-            if (grid[x, y] == "|" 
-                or (grid[x, y] == "L" and next_turn((x, y)) in "7S")
-                or (grid[x, y] == "F" and next_turn((x, y)) in "JS")):
+            if grid[x, y] in step_over_chars:
                 inside_loop = not inside_loop
         elif inside_loop:
             part2 += 1
